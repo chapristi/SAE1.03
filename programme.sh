6c5403ea51a79ae26aaf0b22e75d4637ca2c682f
louis <louis@louis-IdeaPad-3-15IML05>
2023-12-02 21:54:29 +0100

    ajout de commentaires

--- programme.sh
+++ programme.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Function to display error messages
 show_error() {
     echo "Error: $1"
 }
 
-# Function to validate date format
 validate_date() {
     local date_str=$1
     date -d "$date_str" >>/dev/null 2>&1
     return $?
 }
 
-# Function to check if a string contains only letters
 validate_alphabetic() {
     local input=$1
     if [[ ! $input =~ ^[a-zA-Z]+$ ]]; then
@@ -21,16 +18,14 @@ validate_alphabetic() {
     fi
 }
 
-# Function to check if the student year is valid
 validate_student_year() {
     local year=$1
-    if [[ $annee -ne 1 && $annee -ne 2 && $annee -ne 3 ]]; then
+    if [[ $year -ne 1 && $year -ne 2 && $year -ne 3 ]]; then
         show_error "L'annee de l'etudiant peut etre 1, 2, ou 3: $year"
         return 1
     fi
 }
 
-# Function to check if the phone number is valid
 validate_phone_number() {
     local phone=$1
     if [[ ! $phone =~ ^0[67]([0-9]{8})$ ]]; then
@@ -40,13 +35,12 @@ validate_phone_number() {
 }
 getLineX() {
     local line=$1
-    local fichier=$2
+    local file=$2
 
-    local result=$(echo "$fichier" | head -n $line | tail -n 1)
+    local result=$(echo "$file" | head -n $line | tail -n 1)
     echo $result
 }
 
-# Function to process each line in the file
 process_line() {
     local line=$1
     local current_line=$2
@@ -58,13 +52,12 @@ process_line() {
     local telephone=$(echo "$line" | cut -d ":" -f 4)
     local annee_naissance=$(echo "$line" | cut -d ":" -f 5)
 
-    # ... Additional validations ...
 
-    # Example of using the validation functions
     validate_alphabetic "$prenom" || non_conforme=1;err=1
     validate_alphabetic "$nom" || non_conforme=1;err=1
     validate_student_year "$annee" || non_conforme=1;err=1
     validate_phone_number "$telephone" || non_conforme=1;err=1
+    #date utilise un format de date different du notre donc on le change
     validate_date "${annee_naissance:3:2}/${annee_naissance:0:2}/${annee_naissance:6:4}" || non_conforme=1;err=1
     
     if [ $err -ne 0 ]
@@ -73,7 +66,6 @@ process_line() {
     fi
 }
 
-# Main script starts here
 
 if [ $# -ne 1 ]; then
     show_error "Vous devez entrez un seul argument"
@@ -83,7 +75,7 @@ elif [ ! -f "$1" ]; then
     exit 1
 fi
 
-# Read the file and remove spaces
+#on supprimme les espaces
 file_content=$(cat "$1" | tr -d " ")
 
 number_of_lines=$(echo "$file_content" | wc -l | cut -d ' ' -f 1 )
@@ -91,7 +83,6 @@ echo $number_of_lines
 non_conforme=0
 
 
-# Loop through each line
 for ((current_line = 1; current_line <= $number_of_lines; current_line++))
 do
     echo "------------Ligne $current_line--------------"
