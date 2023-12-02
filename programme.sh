3b3fd0e558da2c9e0f28d88ff358032c086fe5a1
louis <louis@louis-IdeaPad-3-15IML05>
2023-12-02 21:47:23 +0100

    push

--- programme.sh
+++ programme.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
-then
-    echo "Vous devez n'entrer qu'un et un seul argument"
-    exit 1
-elif [ ! -f $1 ]
-then
-    echo "Le fichier n'a pas etait trouvé"
-    exit 1
-fi
+# Function to display error messages
+show_error() {
+    echo "Error: $1"
+}
 
+# Function to validate date format
+validate_date() {
+    local date_str=$1
+    date -d "$date_str" >>/dev/null 2>&1
+    return $?
+}
+
+# Function to check if a string contains only letters
+validate_alphabetic() {
+    local input=$1
+    if [[ ! $input =~ ^[a-zA-Z]+$ ]]; then
+        show_error "The input must contain only letters: $input"
+        return 1
+    fi
+}
+
+# Function to check if the student year is valid
+validate_student_year() {
+    local year=$1
+    if [[ $annee -ne 1 && $annee -ne 2 && $annee -ne 3 ]]; then
+        show_error "The student year must be 1, 2, or 3: $year"
+        return 1
+    fi
+}
+
+# Function to check if the phone number is valid
+validate_phone_number() {
+    local phone=$1
+    if [[ ! $phone =~ ^0[67]([0-9]{8})$ ]]; then
+        show_error "Invalid phone number format: $phone"
+        return 1
+    fi
+}
 getLineX() {
     local line=$1
     local fichier=$2
@@ -18,76 +46,56 @@ getLineX() {
     echo $result
 }
 
-createLoginName() {
-    local nom=$1
-    local prenom=$2
-    #0:1 veut dire on part de l'index 0 et on prend 1 caractere
-    local result="${prenom:0:1}-$nom"
-    echo $result
-}
-#createPassword(){}
-#on recupere le fichier et on supprimme tout les espaces
-fichier=$(cat $1 | tr -d " ")
-#si on met pas les guillements sur fichier cela enleve les retours à la ligne donc mauvais resultat
-taille_fichier=$(echo "$fichier" | wc -l | cut -d ' ' -f 1 )
-non_conforme=0
-#verifier que le fichier n'est pas corrompu
+# Function to process each line in the file
+process_line() {
+    local line=$1
+    local current_line=$2
+    local err=0
 
-for (( i=1; i<=$taille_fichier; i++ ))
-do
-    echo "------------Ligne $i--------------"
-    line=$(getLineX $i "$fichier")
-    nombre_elements=$(echo $line |tr ":" "\n" |wc -l |cut -d ' ' -f 1 )
-    if [ $nombre_elements -ne 5 ]
+    local prenom=$(echo "$line" | cut -d ":" -f 1)
+    local nom=$(echo "$line" | cut -d ":" -f 2)
+    local annee=$(echo "$line" | cut -d ":" -f 3)
+    local telephone=$(echo "$line" | cut -d ":" -f 4)
+    local annee_naissance=$(echo "$line" | cut -d ":" -f 5)
+
+    # ... Additional validations ...
+
+    # Example of using the validation functions
+    validate_alphabetic "$prenom" || non_conforme=1;err=1
+    validate_alphabetic "$nom" || non_conforme=1;err=1
+    validate_student_year "$annee" || non_conforme=1;err=1
+    validate_phone_number "$telephone" || non_conforme=1;err=1
+    validate_date "${annee_naissance:3:2}/${annee_naissance:0:2}/${annee_naissance:6:4}" || non_conforme=1;err=1
+    
+    if [ $err -ne 0 ]
     then
-        echo "Nombre d'elements insuffisant, il n'y a que seulement $nombre_elements elements distinctifs"
-        non_conforme=1
-
-    else
-        prenom=$(echo "$line" | cut -d ":" -f 1)
-        nom=$(echo "$line" | cut -d ":" -f 2)
-        annee=$(echo "$line" | cut -d ":" -f 3)
-        telephone=$(echo "$line" | cut -d ":" -f 4)
-        annee_naissance=$(echo "$line" | cut -d ":" -f 5)
-        erreur=0
-        #verifier que le prenom ne contient que des lettres
-        if [[ ! $prenom =~ ^[a-zA-Z]+$ ]]
-        then
-            echo "La partie du prenom ne doit contenir que des lettres"
-            erreur=1
-        fi
-        #verifier que le nom ne contient que des lettres
-        if [[ ! $nom =~ ^[a-zA-Z]+$ ]]
-        then
-            echo "La partie du nom ne doit contenir que des lettres"
-            erreur=1
-        fi
-        #verifier que l'annee se trouve dans l'intervalle [1,3]
-        if [[ $annee -ne 1 && $annee -ne 2 && $annee -ne 3 ]]
-        then
-            echo "L'année de l'eleve doit etre 1 2 ou 3"
-            erreur=1
-        fi
-        #verifier que le numero de telephone soit correcte
-        if [[  ! $telephone =~ ^0[67]([0-9]{8})$ ]]
-        then
-            echo "Le numero de telephone de l'etudiant doit etre conforme"
-            erreur=1
-        fi
-        annee_est_valide=$(date -d "${annee_naissance:3:2}/${annee_naissance:0:2}/${annee_naissance:6:4}" >/dev/null 2>&1)
-        #verifier que la date est valide
-        if [[ $? -ne 0 ]]
-        then
-            echo "La date de naissance doit etre valide"
-            erreur=1
-        fi
-        if [[ $erreur -eq 0 ]]
-        then
-            echo "Rien à signaler pour cette ligne"
-        else
-            non_conforme=1
-        fi
+        echo "Line $current_line is valid."
     fi
+}
+
+# Main script starts here
+
+if [ $# -ne 1 ]; then
+    show_error "You must enter exactly one argument."
+    exit 1
+elif [ ! -f "$1" ]; then
+    show_error "File not found: $1"
+    exit 1
+fi
+
+# Read the file and remove spaces
+file_content=$(cat "$1" | tr -d " ")
+
+number_of_lines=$(echo "$file_content" | wc -l | cut -d ' ' -f 1 )
+echo $number_of_lines
+non_conforme=0
+
+
+# Loop through each line
+for ((current_line = 1; current_line <= $number_of_lines; current_line++)); do
+    echo "------------Line $current_line--------------"
+    current_line_content=$(getLineX "$current_line" "$file_content")
+    process_line "$current_line_content" "$current_line"
 done
 
 if [ $non_conforme -eq 1 ]
