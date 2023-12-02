f6c847f77bf5bbdea9d63599e92bb2979be9ca39
louis <louis@louis-IdeaPad-3-15IML05>
2023-12-02 21:17:27 +0100

    push

--- programme.sh
+++ programme.sh
@@ -22,7 +22,7 @@ createLoginName() {
     local nom=$1
     local prenom=$2
     #0:1 veut dire on part de l'index 0 et on prend 1 caractere
-    local result="${nom:0:1}-$nom"
+    local result="${prenom:0:1}-$nom"
     echo $result
 }
 #createPassword(){}
@@ -48,29 +48,32 @@ do
         telephone=$(echo "$line" | cut -d ":" -f 4)
         annee_naissance=$(echo "$line" | cut -d ":" -f 5)
         erreur=0
+        #verifier que le prenom ne contient que des lettres
         if [[ ! $prenom =~ ^[a-zA-Z]+$ ]]
         then
             echo "La partie du prenom ne doit contenir que des lettres"
             erreur=1
         fi
+        #verifier que le nom ne contient que des lettres
         if [[ ! $nom =~ ^[a-zA-Z]+$ ]]
         then
             echo "La partie du nom ne doit contenir que des lettres"
             erreur=1
         fi
+        #verifier que l'annee se trouve dans l'intervalle [1,3]
         if [[ $annee -ne 1 && $annee -ne 2 && $annee -ne 3 ]]
         then
             echo "L'année de l'eleve doit etre 1 2 ou 3"
             erreur=1
-
         fi
+        #verifier que le numero de telephone soit correcte
         if [[  ! $telephone =~ ^0[67]([0-9]{8})$ ]]
         then
             echo "Le numero de telephone de l'etudiant doit etre conforme"
             erreur=1
         fi
-        echo ${annee_naissance:6:4}
         annee_est_valide=$(date -d "${annee_naissance:3:2}/${annee_naissance:0:2}/${annee_naissance:6:4}" >/dev/null 2>&1)
+        #verifier que la date est valide
         if [[ $? -ne 0 ]]
         then
             echo "La date de naissance doit etre valide"
